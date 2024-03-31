ba6b66441d06d6dfd4dc76feed093700e314ba89
Kelven Bango <Kelvenbango@gmail.com>
2024-03-31 08:48:48 -0700

    feat: added save() method from FileSTorage module

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,12 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(
                     kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(
                     kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            
             self.__dict__.update(kwargs)
 
 
@@ -28,6 +28,7 @@ class BaseModel:
     def save(self):
         """updates the attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__"""
