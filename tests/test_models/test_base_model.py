def9636246a1aed18b82a445aa85df4d7a181164
Kelven Bango <Kelvenbango@gmail.com>
2024-03-31 06:30:57 -0700

    docs: added new test cases for the new features added on BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,17 +12,18 @@ class test_basemodel(unittest.TestCase):
         self.model = BaseModel()
 
     def tearDown(self):
+        """"""
         pass
 
-    def test_id_initialization(self):
+    def test_id(self):
         """Test that id is initialized properly"""
         self.assertIsNotNone(self.model.id)
     
-    def test_created_at_initialization(self):
+    def test_created_at(self):
         """Test created_at attributte is initialized properly"""
         self.assertIsInstance(self.model.created_at, datetime)
 
-    def test_updated_at_initialization(self):
+    def test_updated_at(self):
         """Test updated_at attributte is initialized properly"""
         self.assertIsInstance(self.model.updated_at, datetime)
 
@@ -50,10 +51,21 @@ class test_basemodel(unittest.TestCase):
                 object_dictionary['created_at'],
                 self.model.created_at.isoformat()
                 )
-        self.assertEqual(
-                object_dictionary['updated_at'],
+        self.assertEqual(object_dictionary['updated_at'],
                 self.model.updated_at.isoformat()
                 )
+
+    def test_init_with_kwargs(self):
+        """Test initialization with keyword arguments"""
+        args = {
+            'id': 'test_id',
+            'created_at': '2024-03-31T12:00:00.000000',
+            'updated_at': '2024-03-31T12:00:00.000000'
+        }
+        model = BaseModel(**args)
+        self.assertEqual(model.id, 'test_id')
+        self.assertEqual(model.created_at, datetime(2024, 3, 31, 12, 0))
+        self.assertEqual(model.updated_at, datetime(2024, 3, 31, 12, 0))
 
 if __name__ == "__main__":
     unittest.main()
